437fa63b778dcbd76559b214e6d1563fec0cce0d
Tim Clark <tim.clark@inforeliance.com>
2015-11-19 17:24:08 -0500

    woot!

---
+++ app/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name myawesomeapp10App
+ * @description
+ * # myawesomeapp10App
+ *
+ * Main module of the application.
+ */
+angular
+  .module('myawesomeapp10App', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
