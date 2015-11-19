53151b0ab15c6858f31346334123e80505a3cc02
Tim Clark <tim.clark@inforeliance.com>
2015-11-19 18:04:11 -0500

    even better buildasdasdasd

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,14 @@
  * Controller of the myawesomeapp10App
  */
 angular.module('myawesomeapp10App')
-  .controller('MainCtrl', function () {
+  .controller('MainCtrl', function ($scope) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+
+    $scope.ShowSweetAlert = function(){
+      swal('Holy cow','This actually worked?!??!','success');
+    };
   });
